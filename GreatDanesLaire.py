159b1224264ff5e6236f3fe886f114b3102ec1ec
Trent Mulligan <tm327434@inf108-fa18.arcc.albany.edu>
2018-11-28 12:18:17 -0500

    I created the first possible scenarios regarding my final assigment

---
+++ GreatDanesLaire.py
@@ -0,0 +1,11 @@
+import random
+import time 
+
+def displayIntro():
+	print('You are on campus and there are zombies running wild.')
+	print('You see two escapes. The library and the Campus Center')
+	print('The campus center is filled with the hungry survivors on campus')
+	print('The libary is empty yet has limited supplies')
+	print()
+
+def chooseCave():
