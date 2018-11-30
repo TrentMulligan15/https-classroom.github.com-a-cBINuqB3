2f6245d97e53549989864de6ba7afafe8702be84
Trent Mulligan <tm327434@inf108-fa18.arcc.albany.edu>
2018-11-30 12:30:14 -0500

    Added an updated file for my final project

---
+++ GreatDanesLaire2.py
@@ -0,0 +1,28 @@
+import random
+import time 
+
+def displayIntro():
+    print("You are on campus and there are zombies running wild.")
+    print("You see two escapes. The library and the Campus Center")
+    print("The campus center is filled with the hungry survivors on campus")
+    print("The libary is empty yet has limited supplies")
+    hidingSpot=input('Which hiding place will you choose?(Library or CampusCenter)'))
+    if hidingSpot == "Library":
+        checkSpot()   
+ 
+def chooseHidingSpot():
+    HidingSpot = ''
+    while cave != "Library" and cave != "CampusCenter"
+    print('Which hiding place will you choose?(Library or CampusCenter)')
+
+         return HidingSpot
+
+def checkHidingSpot(chosenHidingSpot):
+    print('You enter you're hiding spot...')
+    time.sleep(2)
+        print('
+
+def checkSpot():
+	
+
+displayIntro()
